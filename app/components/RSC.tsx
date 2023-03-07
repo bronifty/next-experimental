16145acec63c49a333f9b660c90ae97c0906e0f9
bronifty <bronifty@gmail.com>
2023-03-07 00:09:19 -0500

    updated api calls

--- app/components/RSC.tsx
+++ app/components/RSC.tsx
@@ -3,24 +3,19 @@ import React from "react";
 const getProduct1 = fetch("https://dummyjson.com/products/1").then((res) =>
   res.json()
 );
-const getProduct2 = fetch("https://dummyjson.com/products/1").then((res) =>
+const getProduct2 = fetch("https://dummyjson.com/products/2").then((res) =>
   res.json()
 );
 
-// const TestRSC = () => {
-//   const product = React.use(getProduct1);
-//   if (!product) return <div>Loading...</div>;
-
-//   return <div>In the RSC created in RSC file: {product.title}</div>;
-// };
 const RSC = () => {
-  const product = React.use(getProduct1);
-  if (!product) return <div>Loading...</div>;
+  const product1 = React.use(getProduct1);
+  const product2 = React.use(getProduct2);
+  if (!product1 || !product2) return <div>Loading...</div>;
 
   return (
     <div>
-      <h2>In the RSC passed as children to the RCC via page.tsx in next: </h2>
-      <div>product.title from use hook query: {product.title}</div>
+      <div> {product1.title}</div>
+      <div> {product2.title}</div>
     </div>
   );
 };
