a5d1f826eab5890769d7e409926143dcee0d079e
bronifty <bronifty@gmail.com>
2023-03-06 23:59:02 -0500

    initial commit next experimental

---
+++ app/components/RCC.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+
+const RCC = ({ children }) => {
+  return (
+    <>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        {/* <div>title passed to Client Child: {props.title}</div> */}
+        {children}
+      </React.Suspense>
+    </>
+  );
+};
+
+export default RCC;
---
+++ app/components/RSC.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import RCC from "./RCC";
+
+const getProduct1 = fetch("https://dummyjson.com/products/1").then((res) =>
+  res.json()
+);
+const getProduct2 = fetch("https://dummyjson.com/products/1").then((res) =>
+  res.json()
+);
+
+// const TestRSC = () => {
+//   const product = React.use(getProduct1);
+//   if (!product) return <div>Loading...</div>;
+
+//   return <div>In the RSC created in RSC file: {product.title}</div>;
+// };
+const RSC = () => {
+  const product = React.use(getProduct1);
+  if (!product) return <div>Loading...</div>;
+
+  return <div>In the RSC created in RSC file: {product.title}</div>;
+};
+
+export default RSC;
